530889acca2e98fc640e54e29456445ce1f71c5c
Hudkoff <alexus.web@gmail.com>
2013-11-26 21:10:34 +0300

    initial commit

---
+++ date.js
@@ -0,0 +1,35 @@
+﻿
+
+
+
+
+
+
+
+(function() {
+
+
+
+	function randomizer(minValue, maxValue) {
+		var rand = minValue + Math.random()*(maxValue+1-minValue); 
+		//rand = rand^0; // some another way to round
+		rand = Math.floor(rand); 
+		return rand;
+	}
+
+	function probabilityControl(iterations) {
+		var x = y = 0;
+		for (var i = 0; i <= iterations; i++) {
+			if (randomizer(1, 2) == 1) {
+				x++;
+			} else {
+				y++;
+			}
+		}
+		console.log(x, "% vs. ", y, "%")
+	}
+
+probabilityControl(1000)
+
+})()
+
